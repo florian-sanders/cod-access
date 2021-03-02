203bbe4e86a23afbf5e67136a74532ab6d169e9d
AnnekimBanchereau <annekim.banchereau@gmail.com>
2021-03-02 18:39:48 +0100

    feat: add aria-required

--- front/src/components/SignUp/index.js
+++ front/src/components/SignUp/index.js
@@ -11,20 +11,20 @@ const SignUp = ({}) => (
       <form action="" method="get" className="signup__content__form">
         <div className="signup__content__form__group">
           <label hmtlFor="email" className="signup__content__form__group__label">Email</label>
-          <input type="email" name="email" id="email" required className="signup__content__form__group__input" />
+          <input type="email" name="email" id="email" className="signup__content__form__group__input" aria-required="true" />
         </div>
         <div className="signup__content__form__group">
           <label hmtlFor="pseudo" className="signup__content__form__group__label">Pseudo</label>
-          <input type="text" name="pseudo" id="pseudo" required className="signup__content__form__group__input" />
+          <input type="text" name="pseudo" id="pseudo" className="signup__content__form__group__input" aria-required="true" />
 
         </div>
         <div className="signup__content__form__group">
           <label hmtlFor="password" className="signup__content__form__group__label">Mot de passe</label>
-          <input type="password" name="password" id="password" required className="signup__content__form__group__input" />
+          <input type="password" name="password" id="password" className="signup__content__form__group__input" aria-required="true" />
         </div>
         <div className="signup__content__form__group">
           <label hmtlFor="password_confirm" className="signup__content__form__group__label">Confirmez votre mot de passe</label>
-          <input type="password" name="password_confirm" id="password_confirm" required className="signup__content__form__group__input" />
+          <input type="password" name="password_confirm" id="password_confirm" className="signup__content__form__group__input" aria-required="true" />
         </div>
         <div className="signup__content__form__group">
           <input type="submit" value="S'enregistrer" className="signup__content__form__submit" />
