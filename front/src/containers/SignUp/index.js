e598dd1e52424be9ee0d9cf023ce991ca4d5b60d
AnnekimBanchereau <annekim.banchereau@gmail.com>
2021-03-03 14:59:40 +0100

    front/feat: control fields signup

---
+++ front/src/containers/SignUp/index.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import SignUp from 'src/components/SignUp';
+import {
+  setFieldValue,
+} from 'src/actions/signup';
+
+const mapStateToProps = (state) => ({
+  email: state.signup.email,
+  pseudo: state.signup.pseudo,
+  password: state.signup.password,
+  passwordConfirm: state.signup.passwordConfirm,
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeField: (value, name) => dispatch(setFieldValue(value, name)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
--- front/src/reducers/signup.js
+++ front/src/reducers/signup.js
@@ -1,9 +1,23 @@
+import {
+  SET_FIELD_VALUE,
+} from 'src/actions/signup';
+
 const initialState = {
-  
+  name: '',
+  value: '',
+  pseudo: '',
+  email: '',
+  password: '',
+  passwordConfirm: '',
 };
 
 export default (state = initialState, action = {}) => {
   switch (action.type) {
+    case SET_FIELD_VALUE:
+      return {
+        ...state,
+        [action.name]: action.value,
+      };
     default:
       return state;
   }
