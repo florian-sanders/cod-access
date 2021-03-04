e63e1101beddfeb3bec8b8a6c670542b612e8b8b
derfy86 <bourigeaudfrederic@yahoo.fr>
2021-03-04 16:21:30 +0100

    back/fix: GET all themes for exercises

--- back/app/controllers/themeController.js
+++ back/app/controllers/themeController.js
@@ -1,13 +1,14 @@
-const { Theme } = require('../models');
+const { Theme, Exercise } = require('../models');
 
 module.exports = {
 
     getAllThemes: async (req, res, next) => {
         try{
-            const theme = await Theme.findByPk(1,{
-                include: ['exercises','docs']
+            const theme = await Theme.findAll({
+                include: [{model:Exercise, as: 'exercises',where:{published: true}}]
               });
             console.log('theme', theme);
+
             return res.status(200).json(
             theme
             );
