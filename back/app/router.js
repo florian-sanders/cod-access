4cc162f3c445da18dfbb5c1ed400ea8e7dbe8470
derfy86 <bourigeaudfrederic@yahoo.fr>
2021-03-04 14:37:40 +0100

    back/feat: GET one client

--- back/app/router.js
+++ back/app/router.js
@@ -4,14 +4,21 @@ const router = express.Router();
 
 const clientController = require('./controllers/clientController');
 const authController = require('./controllers/authController');
+const exerciseController = require('./controllers/exerciseController');
 
 router.route('/clients')
-    .get(clientController.getClients)
+    .get(clientController.getAllClients);
+
+router.route('/profile/:id')
+    .get(clientController.getOneClient);
 
 router.route('/signin')
-    .post(authController.submitLoginForm)
+    .post(authController.submitLoginForm);
 
 router.route('/signup')
-    .post(authController.submitSignupForm)
+    .post(authController.submitSignupForm);
+
+router.route('/exercises')
+    .get(exerciseController.getAllExercises);
 
 module.exports = router;
