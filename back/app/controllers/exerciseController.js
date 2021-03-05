24e28fda358aae0ea185362ef63db312fa0e0eb6
jeremy-YVON <jeremy.yvon@outlook.fr>
2021-03-05 11:37:30 +0100

    back/feat:api-get-one-exercise

--- back/app/controllers/exerciseController.js
+++ back/app/controllers/exerciseController.js
@@ -4,7 +4,9 @@ module.exports = {
 
     getAllExercises: async (req, res, next) => {
         try{
-            const exercises = await Exercise.findAll();
+            const exercises = await Exercise.findAll({
+                include: ['kind','clients','themes']
+              });
             console.log('exercises', exercises);
             return res.status(200).json(
             exercises
@@ -15,4 +17,25 @@ module.exports = {
         }
     },
 
+    getOneExercise: async (req, res, next) => {
+        try{
+            //const id = Number(req.user.exerciseId);
+            const id = Number(req.params.id);
+            if (isNaN(id)) {
+                return res.status(400).json({
+                    error: `the provided id must be a number`
+                });
+            }
+            const exercise = await Exercise.findByPk(id, {
+                include: ['kind','clients','themes']
+              });
+            console.log('exercise', exercise);
+            return res.status(200).json(
+            exercise
+            );
+        } catch(error) {
+            console.error(error);
+            return res.status(500);
+        }
+    },
 }
