94b96f0efb659c425bb8abb733289fd2e20c8278
Florian Sanders <sanders.florian@gmail.com>
2021-03-06 18:16:03 +0100

    back/feat: add csurf middleware for CSRF protection

--- back/index.js
+++ back/index.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const router = require('./app/router');
+const csrf = require('csurf');
+
+
+const cookieParser = require('cookie-parser');
+
 const bodyParser = require('body-parser');
 const pathToSwaggerUi = require('swagger-ui-dist').absolutePath()
 console.log('process.env.PG_URL', process.env.PG_URL)
@@ -10,11 +15,23 @@ const cors = require('cors');
 const corsOptions = {
     origin: 'http://localhost:8080',
 };
+
 app.use(cors(corsOptions));
+
+app.use(cookieParser());
+
+// use double submit cookie policy
+const csrfProtection = csrf({
+    cookie: true
+});
+
+// for every request, check whether csrf cookie token value and request header token value match
+// if they match, will go to next middleware, if not, will throw an error
+app.use(csrfProtection);
 
 app.use(bodyParser.json());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // app.use(express.static(__dirname + '/assets'));
 app.use(express.static(pathToSwaggerUi));
