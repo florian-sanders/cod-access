276b81d483ec337eba0f69f31b49150982b8fc3f
AnnekimBanchereau <71634072+AnnekimBanchereau@users.noreply.github.com>
2021-03-08 15:30:46 +0100

    Revert "Front/filter theme final"

--- back/migration/revert/file_bdd.sql
+++
@@ -1,8 +0,0 @@
--- Revert create_db:file_bdd from pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-DROP TABLE exercise_theme, theme_doc, client_doc, client_exercise, possible_answer, theme, doc, question, exercise, kind, client, picture, responsibility;
-
-COMMIT;
