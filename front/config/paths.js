fd08adf3bcf874d040fc8db83583bd1978453e85
Florian Sanders <sanders.florian@gmail.com>
2021-03-09 08:52:24 +0100

    front/fix: change path for yarn build, was missing one ../

--- front/config/paths.js
+++ front/config/paths.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   src: path.resolve(__dirname, '../src'), // source files
   assets: path.resolve(__dirname, '../src/assets'), // assets files
-  build: path.resolve(__dirname, '../back/assets'), // production build files
+  build: path.resolve(__dirname, '../../back/assets'), // production build files
   static: path.resolve(__dirname, '../public'), // static files to copy to build folder
   envPath: path.resolve(__dirname,
     path.join('../', process.env.NODE_ENV === 'production' ? '.env.public.prod' : '.env.public.dev')
