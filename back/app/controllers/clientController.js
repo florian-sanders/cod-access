52afc7106d5f65447d04d0ed5ca6d8aa0d97a710
AnnekimBanchereau <annekim.banchereau@gmail.com>
2021-03-11 18:52:08 +0100

    back/fix: fix condition client controller

--- back/app/controllers/clientController.js
+++ back/app/controllers/clientController.js
@@ -75,7 +75,7 @@ module.exports = {
                     error: `the provided id must be a number`
                 });
             }
-            if(req.body.responsibility === 'client'){
+            if(req.body.responsibility === 'utilisateur'){
                 req.body.responsibility = 1
             }else if(req.body.responsibility === 'admin'){
                 req.body.responsibility = 2
