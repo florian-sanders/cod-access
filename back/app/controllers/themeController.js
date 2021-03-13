d962b08cef232524ce50b3f5537cb795d68fe2bf
AnnekimBanchereau <annekim.banchereau@gmail.com>
2021-03-13 14:03:07 +0100

    back/perf:get all themes exercises

--- back/app/controllers/themeController.js
+++ back/app/controllers/themeController.js
@@ -8,17 +8,28 @@ module.exports = {
             if (req.user) {
                 myClient = req.user.clientId
             }
-            const theme = await Theme.findAll({
+            const themes = await Theme.findAll({
                 include: [
-                    {model:Exercise, as: 'exercises',where:{published: true}},
+                    {
+                        model:Exercise, as: 'exercises',
+                        where:{published: true},
+                        attributes: { exclude: ['brief', 'published'] },
+                    },
                     {
                         association: 'exercises',
-                        include: [ {model: Client, as: 'clients', where: { id: myClient }, required: false}],
+                        include: [ 
+                            {
+                                model: Client, as: 'clients', where: { id: myClient },
+                                required: false,
+                                attributes: { exclude: ['password', 'email', 'pseudo', 'responsibility_id', 'picture_id'] },
+                            },
+                            'themes',
+                        ],
                     },
                 ]
             });
             return res.status(200).json(
-            theme
+                themes
             );
             
         } catch(error) {
