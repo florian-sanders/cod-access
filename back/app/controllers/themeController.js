929e782c0297745e93774b9384d835f800eeec6a
derfy86 <bourigeaudfrederic@yahoo.fr>
2021-03-15 13:37:44 +0100

    back/feat: progress

--- back/app/controllers/themeController.js
+++ back/app/controllers/themeController.js
@@ -10,22 +10,16 @@ module.exports = {
             }
             const themes = await Theme.findAll({
                 include: [
-                    // {
-                    //     model:Exercise, as: 'exercises',
-                    //     where:{published: true},
-                    //     attributes: { exclude: ['brief', 'published'] },
-                    // },
                     {
                         association: 'exercises',
                         where:{published: true},
-                        attributes: { exclude: ['brief', 'published'] },
+                        attributes: { exclude: ['brief', 'published', 'exercise_theme'] },
                         include: [ 
                             {
                                 model: Client, as: 'clients', where: { id: myClient },
                                 required: false,
-                                attributes: { exclude: ['password', 'email', 'pseudo', 'responsibility_id', 'picture_id'] },
+                                attributes: { exclude: ['password', 'responsibility_id', 'picture_id'] },
                             },
-                            'themes',
                         ],
                     },
                 ]
@@ -47,6 +41,54 @@ module.exports = {
             theme
             );
 
+        } catch(error) {
+            console.error(error);
+            return res.status(500);
+        }
+    },
+    
+    getScoreByTheme: async (req, res, next) => {
+        try{
+            let myClient = null;
+            if (req.user) {
+                myClient = req.user.clientId
+            }
+            const themes = await Theme.findAll({
+                include: [
+                    {
+                        association: 'exercises',
+                        where:{published: true},
+                        attributes: { exclude: ['brief', 'published', 'exercise_theme'] },
+                        include: [ 
+                            {   
+                                model: Client, as: 'clients', where: { id: myClient },
+                                required: false,
+                                attributes: { exclude: ['password', 'responsibility_id', 'picture_id'] },
+                            },
+                        ],
+                    },
+                ]
+            });
+            let progressUser = [];
+            for (const theme of themes){
+                const numberOfExercises = theme.exercises.length
+                let exerciseComplete = 0;
+                for (const exercise of theme.exercises){
+                    if(exercise.clients[0]){
+                        const score = exercise.clients[0].Client_exercise.score
+                        if(score === 100){
+                            exerciseComplete++
+                        }
+                    }
+                }
+                const progress = Math.round((exerciseComplete/numberOfExercises)*100) 
+                progressUser.push({theme: theme.name, progress: progress})
+            }
+            console.log('progressUser', progressUser)
+            return res.status(200).json(
+                progressUser
+            );
+            
         } catch(error) {
             console.error(error);
             return res.status(500);
