ec1ee3f0d9c7e0bcecb5a03acc1660fa725bc5ef
Florian Sanders <sanders.florian@gmail.com>
2021-03-17 19:13:33 +0100

    front/feat: responsive menu and connection

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -7,10 +7,8 @@ import logoPath from 'src/assets/img/logo.svg';
 const Header = () => (
   <header className="header-wrapper__header" role="banner">
     <Link className="header-wrapper__header__link" to="/">
-      <img className="header-wrapper__header__link__site-logo" src={logoPath} alt="Retour à l'accueil" />
-      {/* <p className="header-wrapper__header__link__site-name">Cod'Access</p> */}
+      <img className="header-wrapper__header__link__site-logo" src={logoPath} alt="Cod'Access - Retour à l'accueil" />
     </Link>
-
   </header>
 );
 
