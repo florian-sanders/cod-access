b5bb5d5e57d656f2f88b4978585460ddfab7c13d
AnnekimBanchereau <annekim.banchereau@gmail.com>
2021-03-17 17:45:51 +0100

    front/style:component Profile not finished

--- front/src/components/Profile/index.js
+++ front/src/components/Profile/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 import ProgressBar from './ProgressBar';
 
 import './styles.scss';
@@ -10,26 +12,33 @@ const Profile = ({
   useEffect(() => {
     fetchProgressByTheme();
   }, []);
-console.log(progressByTheme)
+
   return (
     <div className="profile">
       <h1 className="title-h1">Mon Profil</h1>
-      <div className="profile__content">
-        <img className="profile__content__img" src={`${process.env.IMAGE}${picturePath}`} alt="" />
-        <div className="profile__content__infos">
-          <h2 className="profile__content__infos__pseudo title-h2">Pseudo : {pseudo}</h2>
-          <h2 className="profile__content__infos__email title-h2">Email : {email}</h2>
+      <div className="wrapper">
+        <div className="profile__content">
+          <img className="profile__content__img" src={`${process.env.IMAGE}${picturePath}`} alt="" />
+          <div className="profile__content__infos">
+            <h2 className="profile__content__infos__pseudo">Pseudo : {pseudo}</h2>
+            <h2 className="profile__content__infos__email">Email : {email}</h2>
+          </div>
         </div>
-      </div>
-      <div className="profile__progress">
-        {
-          progressByTheme.map((progress) => (
-            <div key={progress.theme} className="profile__progress__theme">
-              <h2 className="title-h2">{progress.theme}</h2>
-              <ProgressBar color={progress.color} percentage={`${progress.progress}%`} />
-            </div>
-          ))
-        }
+        <div className="profile__progress">
+          {
+            progressByTheme.map((progress) => (
+              <div key={progress.theme} className="profile__progress__theme">
+                <div className="profile__progress__theme__header">
+                  <h2 className="profile__progress__theme__header__title">{progress.theme}</h2>
+                  {
+                    progress.progress === 100 && <FontAwesomeIcon icon={faTrophy} size="2x" />
+                  }
+                </div>
+                <ProgressBar color={progress.color} percentage={`${progress.progress}%`} />
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
