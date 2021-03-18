11e4412c3620b2191831b1c52af79965595ba508
Fred Derfy <bourigeaudfrederic@yahoo.fr>
2021-03-18 15:34:58 +0100

    front/feat: admin mobile

--- front/src/components/AdminMenu/index.js
+++ front/src/components/AdminMenu/index.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faFileAlt, faTools } from '@fortawesome/free-solid-svg-icons';
 import './styles.scss';
 
 const AdminMenu = ({ }) => (
   <div className="nav__menu">
   <nav role="navigation" aria-label="admin">
     <ul className="admin__menu">
-      <p className="title-h2">Dashboard</p>
+      <p className="title-h2 admin__menu hidden-mobile" >Dashboard</p>
       <li>
-        <FontAwesomeIcon icon={faUserCircle} size="2x" />
         <NavLink className="admin__menu__link" to="/admin/utilisateurs" exact activeClassName="admin__menu__link--active">
-          Utilisateurs
+         <FontAwesomeIcon icon={faUserCircle} size="2x" /> 
+         <p className="hidden-mobile">Utilisateurs</p>
         </NavLink>
       </li>
       <li>
-        <FontAwesomeIcon icon={faUserCircle} size="2x" />
         <NavLink className="admin__menu__link" to="/admin/exercices" exact activeClassName="admin__menu__link--active">
-          Exercices
+         <FontAwesomeIcon icon={faFileAlt} size="2x" />
+         <p className="hidden-mobile">Exercices</p>
         </NavLink>
       </li>
       <li>
-        <FontAwesomeIcon icon={faUserCircle} size="2x" />
         <NavLink className="admin__menu__link" to="/admin/creer-exercice" exact activeClassName="admin__menu__link--active">
-          Créer un exercice
+         <FontAwesomeIcon icon={faTools} size="2x" />
+         <p className="hidden-mobile">Créer un exercice</p>
         </NavLink>
       </li>
     </ul>
