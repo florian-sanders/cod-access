be94ea6fafb4135a1e22198a53bf3772dc2a42da
jeremy-YVON <jeremy.yvon@outlook.fr>
2021-03-18 17:18:49 +0100

    front/css:plan-du-site

--- front/src/components/Sitemap/index.js
+++ front/src/components/Sitemap/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useImperativeHandle } from 'react';
 
 import './styles.scss';
 
 const Sitemap = () => (
-  <section>
-    <h1>Plan du site</h1>
+  <section className="Sitemap">
+
+    <h1 className="Sitemap__title">Plan du site</h1>
+
+    <h2 className="Sitemap__subTitle">Navigation Header</h2>
+
+    <ul>
+      <li><a href="/">Accueil</a></li>
+      <li><a href="/challenges">Challenges</a></li>
+      <li><a href="/a-propos">À propos</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+
+    <h2 className="Sitemap__subTitle">Gestion du compte</h2>
+    <ul>
+      <li><a href="/oubli-mot-de-passe">Mot de passe oublié</a></li>
+      <li><a href="/inscription">Inscription</a></li>
+    </ul>
+
+    <h2 className="Sitemap__subTitle">Navigation Footer</h2>
+    <ul>
+      <li><a href="/mentions-legales">Mentions légales</a></li>
+      <li><a href="/plan-du-site">Plan du site</a></li>
+    </ul>
+
   </section>
 );
 
