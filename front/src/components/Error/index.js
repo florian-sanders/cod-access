f6f3fdf1c4df1def0640e6e9d4325512a184fa6f
Fred Derfy <bourigeaudfrederic@yahoo.fr>
2021-03-21 11:30:22 +0100

    front/fix: 404

--- front/src/components/Error/index.js
+++ front/src/components/Error/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ErrorPage from 'src/assets/img/404-short.svg'
+import ErrorPage from 'src/assets/img/404.svg'
 import './styles.scss';
 
 const Error = () => {
     return(
-        <>
-            <img className="errorPage" src={ErrorPage} alt="error-image" />
-        </>
+        <div className="errorPage">
+            <img className="errorPage__picture" src={ErrorPage} alt="error-image" />
+        </div>
     );
 };
 
