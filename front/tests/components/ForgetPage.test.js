751fecda916ceff34440fefbeedd3b0ab9a444ef
derfy86 <bourigeaudfrederic@yahoo.fr>
2021-04-16 11:03:56 +0200

    front/feat: TU front works (linux only)

--- front/tests/components/ForgetPage.test.js
+++ front/tests/components/ForgetPage.test.js
@@ -5,11 +5,14 @@ import { shallow } from 'enzyme';
 import ForgetPage from 'src/components/ForgetPage';
 
 describe('ForgetPage Component', () => {
-    const wrapper = shallow(<ForgetPage />);
+  const wrapper = shallow(<ForgetPage email="lorem@ipsum.com" />);
 
-    it('should have a className forget', () => {
-        expect(wrapper.props().className).to.be.equal('content');
-        // © Diane
-        expect(wrapper.hasClass('forget')).to.be.true;
-      });
-})
+  it('should have 1 section inside', () => {
+    expect(wrapper.find('section')).to.have.lengthOf(1);
+  });
+
+  it('should have a className forget', () => {
+    expect(wrapper.props().className).to.be.equal('forget');
+    expect(wrapper.hasClass('forget')).to.be.true;
+  });
+});
