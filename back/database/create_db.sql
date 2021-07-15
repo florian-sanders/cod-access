38880d353d4874d606188b20b27f0149d518da33
Florian Sanders <sanders.florian@gmail.com>
2021-07-15 19:26:52 +0200

    back/feat: moved to mysql for o2switch hosting

--- back/database/create_db.sql
+++ back/database/create_db.sql
@@ -1,97 +1,95 @@
--- Deploy create_db:file_bdd to pg
-
 BEGIN;
 
 -- XXX Add DDLs here.
 DROP TABLE IF EXISTS client, responsibility, picture, question, kind, exercise, theme, possible_answer, client_exercise, exercise_theme;
 
 CREATE TABLE responsibility (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     entitled text NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE picture (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name" text,
-    "path" text NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name text,
+    path text NOT NULL,
     alternative text,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE client (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email text NOT NULL,
     pseudo text NOT NULL,
-    "password" text NOT NULL,
-    responsibility_id int REFERENCES responsibility(id) NOT NULL,
-    picture_id int REFERENCES picture(id) ON DELETE SET DEFAULT,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    password text NOT NULL,
+    responsibility_id int NOT NULL REFERENCES responsibility(id),
+    picture_id int REFERENCES picture(id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE kind (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name" text NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name text NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE exercise (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title text NOT NULL,
     brief text,
     slug text,
     published boolean NOT NULL,
     kind_id int REFERENCES kind(id) ON DELETE SET NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE question (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     brief text,
     code text NOT NULL,
     explanation text NOT NULL,
     exercise_id int REFERENCES exercise(id) ON DELETE SET NULL,
     picture_id int REFERENCES picture(id) ON DELETE SET NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE theme (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name" text NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name text NOT NULL,
     color text NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE possible_answer (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     content text NOT NULL,
     correct boolean NOT NULL,
     question_id int REFERENCES question(id) ON DELETE SET NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE client_exercise (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     score int,
     client_id int REFERENCES client(id) ON DELETE CASCADE,
     exercise_id int REFERENCES exercise(id) ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
 CREATE TABLE exercise_theme (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     exercise_id int REFERENCES exercise(id) ON DELETE CASCADE,
     theme_id int REFERENCES theme(id) ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP
 );
 
--- back/migration/revert/file_bdd.sql
+++
@@ -1,8 +0,0 @@
--- Revert create_db:file_bdd from pg
-
-BEGIN;
-
--- XXX Add DDLs here.
-DROP TABLE exercise_theme, client_exercise, possible_answer, theme, question, exercise, kind, client, picture, responsibility;
-
-COMMIT;
